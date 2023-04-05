4fa7791d91532c10627cb27f8efddf1f57b62346
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-04-05 13:07:24 -0400

    commit #2

--- src/store/slices/UsersSlice.js
+++ src/store/slices/UsersSlice.js
@@ -1,1 +1,13 @@
 import {createSlice} from '@reduxjs/toolkit'
+
+const UsersSlice = createSlice({
+    name:"userslice",
+    initialState:{
+        data: []
+    },
+    reducers:{
+
+    }
+})
+
+export const usersReducer = UsersSlice.reducer;
