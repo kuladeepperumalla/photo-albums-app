e026f0d43aaf0b72e960de1498a6f18eb4aaef5c
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-04-08 17:07:15 -0400

    commit #5

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,15 +1,26 @@
-import { useEffect } from "react"
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
-function UsersList (){
-    const dispatch = useDispatch();
+function UsersList() {
+  const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
 
-    useEffect(() => {
-        dispatch(fetchUsers())
-    },[dispatch])
-    return <div>Userslist</div>
-    
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+  if (error) {
+    return <div>error!!!</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
-export default UsersList
+export default UsersList;
